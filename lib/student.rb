a95114f04d6d98538854ea218dfdd1ef32513393
Jesse Byers <jesse.s.byers@gmail.com>
2019-10-09 12:30:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,9 +9,6 @@ class Student
     @name = student_hash[:name]
     @location = student_hash[:location]
     @@all << self
-    # binding.pry
-
-
   end
 
   def self.create_from_collection(students_array)
